0291032d00a50262450604441883ba1f2f07ed36
Zaki Dema Barkah <48666441+zakidem@users.noreply.github.com>
2019-12-19 20:15:12 +0700

    Update Access dictionary.py

--- Course/01-Intermediate Python for Data Science/Chapter 2 Dictionaries & Pandas/Access dictionary.py
+++ Course/01-Intermediate Python for Data Science/Chapter 2 Dictionaries & Pandas/Access dictionary.py
@@ -1,1 +1,8 @@
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+europe.keys()
+# Print out the keys in europe
+print(europe.keys())
 
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
